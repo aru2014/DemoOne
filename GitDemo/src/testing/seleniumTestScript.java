a5225eead412fc352007048e2b092b51d965fee5
preeti <preeti@DESKTOP-3EQS8UC>
2016-05-01 15:16:55 -0700

    second change

--- GitDemo/src/testing/seleniumTestScript.java
+++ GitDemo/src/testing/seleniumTestScript.java
@@ -5,6 +5,7 @@ public class seleniumTestScript {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("y trial");
+ System.out.println("N trial");
 	}
 
 }
